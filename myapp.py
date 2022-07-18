a4245a0939021f435531031f241b333322e41cd1
killer150399 <nk1880571@gmail.com>
2022-07-19 00:27:02 +0530

    added postgresSQL

--- myapp.py
+++ myapp.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from flask import Flask, request
 from flask_restful import Api
@@ -10,8 +11,13 @@ from resources.item import Items, Itemlist
 from resources.store import Store, StoreList
 # while importing any python program it runs it or check it in case of oop thus to control that we use __name__ == '__main__'
 app = Flask(__name__)
+
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABASE_URL', 'sqlite:///data.db')
+    uri, 'sqlite:///data.db')
 # sqlite can be changed with any db like mysql etc
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # extention of flask Sqlalchemy tracker is turned off
